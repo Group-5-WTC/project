722fc2237b7ab99704f3eadaf623399f2a3ea48f
Herman Lintvelt <herman.lintvelt@gmail.com>
2024-11-06 12:49:31 +0200

    deleting multiple output files in prepare.sh

--- dataengineering/de-ex-week3/prepare.sh
+++ dataengineering/de-ex-week3/prepare.sh
@@ -1,18 +1,27 @@
+#!/bin/bash
 cd $1
 
 echo "Removing old output files in dir $1"
 
-file1="local_sqlite_database.db"
-
-if [ -f "$file1" ]; then
-    echo "File $file1 exists. Deleting..."
-    rm "$file1"
-    if [ $? -eq 0 ]; then
-        echo "File $file1 has been successfully deleted."
+remove_file_if_exists() {
+    local file="$1"
+    if [ -f "$file" ]; then
+        echo "File $file exists. Deleting..."
+        rm "$file"
+        if [ $? -eq 0 ]; then
+            echo "File $file has been successfully deleted."
+        else
+            echo "Failed to delete $file."
+        fi
     else
-        echo "Failed to delete $file1."
+        echo "File $file does not exist."
     fi
-else
-    echo "File $file1 does not exist."
-fi
+}
 
+# List of files to delete
+files=("local_sqlite_database.db" "stores.json" "output.csv" "users.csv" "submit.dump")
+
+# Loop through each file and call the function
+for file in "${files[@]}"; do
+    remove_file_if_exists "$file"
+done
